4560d910c68d4a9bd60c11e7ba5b634c72bab9c9
sekar N <sekarnagarajan1234@gmail.com>
2023-09-29 15:01:02 +0530

    java

--- Tic Tac Toe Game In Java/Game.java
+++ Tic Tac Toe Game In Java/Game.java
@@ -23,10 +23,35 @@ class TicTacToe {
             System.out.println("-------------");
         }
     }
+
+    void placeMark(int row, int col, char mark){
+        if(row>=0 && row<=2 && col>=0 && col<=2){
+            board[row][col] = mark;
+        }else {
+            System.out.println("Invalid Position");
+        }
+    }
+    boolean checkColWin(){
+      for (int j = 0;j<=2;j++){
+          if(board[0][j] == board[1][j]&&
+                  board[1][j] == board[2][j]){
+              return true;
+          }
+      }
+      return false;
+    }
 }
 class Game {
     public static void main(String[] args) {
         TicTacToe t = new TicTacToe();
         t.displayBoard();
+        t.placeMark(0,0,'X');
+        t.placeMark(0,1,'O');
+        t.placeMark(2,2,'X');
+        t.placeMark(1,1,'O');
+        t.placeMark(0,2,'X');
+        t.placeMark(2,1,'O');
+        t.displayBoard();
+        System.out.println(t.checkColWin());
     }
 }
